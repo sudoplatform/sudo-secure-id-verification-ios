c81221e75a90d57de9a17f820d314e518de884de
Sudo Platform Engineering <sudoplatform-engineering@anonyome.com>
2025-03-10 15:03:03 +1000

    Release 17.0.0

---
+++ Package.swift
@@ -0,0 +1,36 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SudoIdentityVerification",
+    platforms: [.iOS(.v15)],
+    products: [
+        .library(
+            name: "SudoIdentityVerification",
+            targets: ["SudoIdentityVerification"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/sudoplatform/sudo-api-client-ios", from: "12.0.0"),
+        .package(url: "https://github.com/sudoplatform/sudo-config-manager-ios", from: "4.0.0"),
+        .package(url: "https://github.com/sudoplatform/sudo-logging-ios", from: "2.0.0"),
+        .package(url: "https://github.com/sudoplatform/sudo-user-ios", from: "17.0.3"),
+        .package(url: "https://github.com/aws-amplify/aws-sdk-ios-spm", exact: "2.36.7"),
+        .package(url: "https://github.com/sudoplatform/aws-mobile-appsync-sdk-ios.git", exact: "3.7.2"),
+    ],
+    targets: [
+        .target(
+            name: "SudoIdentityVerification",
+            dependencies: [
+                .product(name: "AWSAppSync", package: "aws-mobile-appsync-sdk-ios"),
+                .product(name: "AWSCore", package: "aws-sdk-ios-spm"),
+                .product(name: "AWSS3", package: "aws-sdk-ios-spm"),
+                .product(name: "SudoLogging", package: "sudo-logging-ios"),
+                .product(name: "SudoConfigManager", package: "sudo-config-manager-ios"),
+                .product(name: "SudoApiClient", package: "sudo-api-client-ios"),
+                .product(name: "SudoUser", package: "sudo-user-ios"),
+            ],
+            path: "SudoIdentityVerification/"),
+    ]
+)
